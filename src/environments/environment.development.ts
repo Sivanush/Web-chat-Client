09f027ab455584ed88c8ec19cabb0bd513b247a8
Sivanush <sivanushedapal@gmail.com>
2024-10-26 02:08:35 +0530

    server url added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,4 @@ export const environment = {
     serverUrl: 'http://localhost:3000/api',
     CLOUDINARY_CLOUD_NAME:'dgpcd5c0d',
     CLOUDINARY_UPLOADPRESET:'CMS-CLOUDINARY',
-    apiUrl:'http://localhost:3000/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+    serverUrlSocket : 'https://web-chat-server-2wdu.onrender.com',
+    serverUrl: 'https://web-chat-server-2wdu.onrender.com/api',
+    CLOUDINARY_CLOUD_NAME:'dgpcd5c0d',
+    CLOUDINARY_UPLOADPRESET:'CMS-CLOUDINARY',
+};
